3c628867a584502fcfdb2e3c587998047db8eadd
ricodm <ricodmaluf@gmail.com>
2014-03-24 11:22:55 -0300

    26-2

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,11 +3,12 @@ class Listing < ActiveRecord::Base
 if Rails.env.development?
 
 	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "noimage.png"
-else
+    else
 	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "noimage.png",
     			  :storage => :dropbox,
     			  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
     			  :path => ":style/:id_:filename"
 
 validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+    end
 end
