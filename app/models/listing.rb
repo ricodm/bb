b627fda9589642e1e14155561a0aaee56694d9cc
ricodm <ricodmaluf@gmail.com>
2014-03-25 10:06:09 -0300

    27

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -13,4 +13,10 @@ if Rails.env.development?
 
 validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
     end
-end
+
+    validates :name, :description, :price, presence: true
+    validates :price, numericality: { greater_than: 0 }
+    validates_attachment_presence :image
+
+    belongs_to :user
+end 
