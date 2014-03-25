b79bcb0cb0d1956a4b63181ae3653ee83f6f7e06
ricodm <ricodmaluf@gmail.com>
2014-03-25 10:20:48 -0300

    27-3

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -18,5 +18,5 @@ validates_attachment_content_type :image, :content_type => ["image/jpg", "image/
     validates :price, numericality: { greater_than: 0 }
     validates_attachment_presence :image
 
-    belongs_to :user
+    belongs_to :user 
 end 
